ba1e81742f10e36e0e6e013f19e069e814ae52f8
markchu <markchu929@gmail.com>
2019-12-10 16:57:19 +0800

    update

---
+++ bot.js
@@ -0,0 +1,30 @@
+// require the discord.js module
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(token);
+
+
+client.on('message', message => {
+	console.log(message.content);
+
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	} else if (message.content.startsWith(`${prefix}test`)) {
+		message.channel.send('Test.');
+	} else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+	
+});
+
