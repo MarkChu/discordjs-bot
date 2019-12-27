302c7f168703b2b419d409fe74655dcd4d5dabc5
markchu <markchu929@gmail.com>
2019-12-27 23:16:54 +0800

    u

--- bot.js
+++ bot.js
@@ -188,11 +188,11 @@ client.on('message', message => {
 			message.channel.send(exampleEmbed);
 			break;
 
-		case 'test'
+		case 'test':
 
 			message.channel.createWebhook("Example Webhook", "https://i.imgur.com/AcRxjxQ.png")
       		.then(webhook => webhook.edit("Example Webhook", "https://i.imgur.com/AcRxjxQ.png")
-        	.then(wb => message.author.send('Here is your webhook https://canary.discordapp.com/api/webhooks/${wb.id}/${wb.token}'))
+        	.then(wb => message.author.send('Here is your webhook https://canary.discordapp.com/api/webhooks/'+wb.id+'/'+wb.token))
         	.catch(console.error))
       		.catch(console.error);
 
