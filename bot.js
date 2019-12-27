43f0dca386bea1c9b7846484ee7fdcedc37388ba
markchu <markchu929@gmail.com>
2019-12-27 23:53:23 +0800

    u

--- bot.js
+++ bot.js
@@ -46,16 +46,26 @@ client.once('ready', () => {
 	});
 
 
-	for (let [snowflake, guildMember] of mems) {
-	  console.log('snowflake: ' + snowflake);
-	  console.log('id: ' + guildMember.id);
-	  console.log('user id: ' + guildMember.user.id);
-	}
+	getUsers();
 
 });
 
 // login to Discord with your app's token
 client.login(token);
+
+
+function getUsers() {
+  let guilds = bot.guilds.array();
+
+  for (let i = 0; i < guilds.length; i++) {
+    bot.guilds.get(guilds[i].id).fetchMembers().then(r => {
+      r.members.array().forEach(r => {
+        let username = `${r.user.username}#${r.user.discriminator}`;
+        console.log(`${username}`);
+      });
+    });
+  }
+}
 
 
 function checkboss(){
