0367ecd7ef3b82bd063937ad65ba663bb347f226
markchu <markchu929@gmail.com>
2019-12-27 23:55:21 +0800

    u

--- bot.js
+++ bot.js
@@ -55,10 +55,10 @@ client.login(token);
 
 
 function getUsers() {
-  let guilds = bot.guilds.array();
+  let guilds = client.guilds.array();
 
   for (let i = 0; i < guilds.length; i++) {
-    bot.guilds.get(guilds[i].id).fetchMembers().then(r => {
+    client.guilds.get(guilds[i].id).fetchMembers().then(r => {
       r.members.array().forEach(r => {
         let username = `${r.user.username}#${r.user.discriminator}`;
         console.log(`${username}`);
